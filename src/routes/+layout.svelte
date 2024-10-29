0e94b7de0a2eee987d66ad4e023609a966b0307d
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-10-29 13:58:20 +0200

    fine tuning the layout
    Signed-off-by: lebogang nkoane <1555646+nkoane@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,9 @@
 </svelte:head>
 <main class="flex h-screen flex-col bg-gray-50">
 	<header class="flex items-center justify-between bg-gray-100 p-8">
-		<h1 class="text-4xl font-bold">Base</h1>
+		<a href="/" class="transition-opacity duration-500 hover:opacity-30">
+			<h1 class="text-4xl font-bold">Base</h1>
+		</a>
 		<nav class="flex gap-4 text-xs uppercase">
 			<a href="/">root</a>
 			<a href="/login">login</a>
@@ -26,3 +28,14 @@
 		<p>{copyright.owner} &copy; {copyright.year}</p>
 	</footer>
 </main>
+
+<style lang="postcss">
+	main header nav a {
+		@apply px-2 py-2 text-blue-500 transition-colors duration-500 hover:bg-blue-100 hover:text-blue-700;
+	}
+
+	:global(button) {
+		@apply rounded-md bg-blue-500 px-2 py-1 text-sm text-white hover:bg-blue-600 active:bg-blue-700 active:outline-2 disabled:bg-gray-300;
+		@apply active:ring-2 active:ring-purple-500;
+	}
+</style>
