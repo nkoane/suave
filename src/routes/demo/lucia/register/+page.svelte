351ccc0ed8bc85bb086b9de0bcd2fdc13a0fa6ab
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-10-29 14:13:55 +0200

    setting up the registration page
    Signed-off-by: lebogang nkoane <1555646+nkoane@users.noreply.github.com>

---
+++ src/routes/demo/lucia/register/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	let { form }: { form: ActionData } = $props();
+</script>
+
+<h1>Register</h1>
+<form method="post" action="?/register" use:enhance>
+	<p class="bg-red-500">
+		<label>
+			Username
+			<input name="username" />
+		</label>
+	</p>
+	<label>
+		Password
+		<input type="password" name="password" />
+	</label>
+	<label>
+		Confirm Password
+		<input type="password" name="confirm-password" />
+	</label>
+	<button>Register</button>
+</form>
+<p style="color: red">{form?.message ?? ''}</p>
